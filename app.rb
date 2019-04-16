17d3a8c39a9a34f4095ae616114c4c56fde96801
Stuart <stuart.pethurst@gmail.com>
2019-04-16 14:40:56 +0100

    added params to ruby code

---
+++ app.rb
@@ -0,0 +1,27 @@
+require "sinatra"
+require "shotgun"
+
+set :session_secret, 'super secret'
+
+get '/' do
+  "Hello World!"
+end
+
+get '/secret' do
+  "This is secret"
+end
+
+get '/nodwink' do
+  "a Nods as good as a Wink to a blind man"
+end
+
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
