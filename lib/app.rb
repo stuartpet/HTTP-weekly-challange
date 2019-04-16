af32177b3c7c7035f11acb9a5e83b33a4dfa0b14
Stuart <stuart.pethurst@gmail.com>
2019-04-16 14:06:29 +0100

    add box to the cat picture and HTML

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,21 @@
 require "sinatra"
 
+set :session_secret, 'super secret'
+
 get '/' do
   "Hello World!"
 end
 
 get '/secret' do
   "This is secret"
+end
+
+get '/nodwink' do
+  "a Nods as good as a Wink to a blind man"
+end
+
+get '/cat' do
+  "<div style='outline: 3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>"
 end
